02810f1e4df2488431ee4cf75eed426ab175c727
pavanann <luccapavansilva@hotmail.com>
2025-05-03 09:11:39 -0300

    pre transfer

---
+++ src/extensions/MangaPlus/index.ts
@@ -0,0 +1,6 @@
+export { searchMangaPlus as search } from './search';
+
+// Giving up for now
+
+// FOR THE FUTURE -> We can or use the v3 api that reuturns all mangas as a UTF-8
+//                      or use Pupeteer (i dont want this)
